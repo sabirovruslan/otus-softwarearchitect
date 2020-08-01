388a1d8744b106c04c88e23810f9e732e41fce4b
Ruslan <sabirov@worksolutions.ru>
2020-08-01 18:43:57 +0300

    HW-1 Сборка контейнера

--- app/app.py
+++ app/app.py
@@ -1,6 +1,3 @@
-import os
-import time
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,16 +8,5 @@ def health():
     return '{"status": "ok"}'
 
 
-@app.route("/version")
-def version():
-    return '{"version": "0.1"}'
-
-
-@app.route("/")
-def hello():
-    return 'Hello world from v2 ' + os.environ['HOSTNAME'] + ' !'
-
-
 if __name__ == "__main__":
-    time.sleep(7)
     app.run(host='0.0.0.0', port='80')
