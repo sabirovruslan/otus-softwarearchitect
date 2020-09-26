e4507d1f41ef4f3c4139fbb673d5857b8499befb
Ruslan <sabirov@worksolutions.ru>
2020-09-26 22:51:45 +0300

    HW-5 BFF, Apigateway, Получение профиля пользователя. Конфигурация чарта

--- hw-5/start.sh
+++ hw-5/start.sh
@@ -9,3 +9,6 @@ kubectl config set-context --current --namespace=myapp
 echo "Helm install auth-app"
 helm dependency update ./auth-app/auth-app-chart
 helm install auth-app ./auth-app/auth-app-chart
+
+echo "Helm install simple-app"
+helm install simple-app ./simple-app/simple-app-chart
