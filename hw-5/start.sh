54081e4804757930a8c2d4f96e6d74dc00158ef4
Ruslan <sabirov@worksolutions.ru>
2020-09-27 00:07:20 +0300

    HW-5 BFF, Apigateway, Подключение api-gateway ambassador

--- hw-5/start.sh
+++ hw-5/start.sh
@@ -6,6 +6,11 @@ kubectl create namespace myapp
 echo "Set context ns 'myapp'"
 kubectl config set-context --current --namespace=myapp
 
+echo "Helm add repo ambassador"
+helm repo add datawire https://getambassador.io
+helm install aes datawire/ambassador -f ambassador_values.yaml
+kubectl apply -f ambassador/
+
 echo "Helm install auth-app"
 helm dependency update ./auth-app/auth-app-chart
 helm install auth-app ./auth-app/auth-app-chart
